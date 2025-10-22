406eca3e4d2d85a3dc0384dfbd3b2883f5ca0928
DmytroHalai <dimagalai04@gmail.com>
2025-10-22 19:38:57 +0300

    Refactor Bus and Car classes to support generics for passenger management

--- src/main/java/org/example/transport/bus/Bus.java
+++ src/main/java/org/example/transport/bus/Bus.java
@@ -1,16 +1,11 @@
+// Bus.java
 package org.example.transport.bus;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.RequiredArgsConstructor;
+import org.example.people.Person;
 import org.example.transport.Transport;
 
-import java.util.Map;
-
-@EqualsAndHashCode(callSuper = true)
-@Data
-@RequiredArgsConstructor
-public class Bus extends Transport {
-    private final int passengerCapacity;
-    private final Map<Integer, Boolean> seatAvailability;
+public class Bus extends Transport<Person> {
+    public Bus(int capacity) {
+        super(capacity);
+    }
 }
--- src/main/java/org/example/transport/car/Car.java
+++ src/main/java/org/example/transport/car/Car.java
@@ -1,11 +1,10 @@
 package org.example.transport.car;
 
+import org.example.people.Person;
 import org.example.transport.Transport;
 
-public class Car extends Transport {
-
-
-    public Car() {
-        super();
+public abstract class Car<T extends Person> extends Transport<T> {
+    protected Car(int capacity) {
+        super(capacity);
     }
 }
